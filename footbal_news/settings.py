9b4d579ec3f8a94d2ebf870ae757117e9c783405
BintoroNtw <bintoronatawijaya@gmail.com>
2025-11-12 21:53:10 +0700

    Tutorial 8 Bintoro Selesai

--- footbal_news/settings.py
+++ footbal_news/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-n-kx6atr9w$n=$=eg9=_$d3_j_bki0r9_y()sc7l49sicpe_gi
 PRODUCTION = os.getenv('PRODUCTION','False').lower()=='true'
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost","127.0.0.1","bintoro-nata-footballnews.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost","127.0.0.1","bintoro-nata-footballnews.pbp.cs.ui.ac.id","10.0.2.2"]
 CSRF_TRUSTED_ORIGINS = [
     "https://bintoro-nata-footballnews.pbp.cs.ui.ac.id"
     #Perbaikan hapus "/" di akhir string
@@ -46,7 +46,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
+    'main',
+    'authentication',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -58,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'footbal_news.urls'
@@ -154,3 +157,10 @@ else:
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
